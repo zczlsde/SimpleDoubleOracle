f8afc2be8b644934aa0f5b12e2225144ff2809d9
Samuel Deng <zczlsde@ucl.ac.uk>
2024-01-28 23:02:15 +0000

    add_games

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ optional arguments:
   -r ROWS, --rows ROWS  The rows of the random game matrix
   -c COLS, --cols COLS  The columns of the random game matrix
   -g, --gambit          Use Gambit to solve LP or Nashpy
+  -m, --games           Select a game to play (5,5-Blotto, tic_tac_toe ...)
 ```
+
+To choose games other than at random, you should download the [payoff](https://proceedings.neurips.cc/paper/2020/hash/ca172e964907a97d5ebd876bfdd4adbd-Abstract.html) from this link and save it in the folder named 'games'.
 
 The double oracle algorithm works as follows:
 
@@ -27,3 +30,11 @@ The double oracle algorithm works as follows:
 Below is a regret plot for an 100*100 zero-sum game solved by this double oracle algorithm:
 
 <img src="plots/Figure_1.png" width="500px">
+
+Below is a regret plot for tic-tac-toe game (Number of strategies: 880):
+
+<img src="plots/Figure_tic_tac_toe.png" width="500px">
+
+Below is a regret plot for connect_four (Number of strategies: 1470):
+
+<img src="plots/Figure_connect_four.png" width="500px">
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from algorithm import double_oracle, double_oracle_with_gambit
 import argparse
 import numpy as np
+import pickle
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-r', '--rows', type=int, default=100, help='The rows of the random game matrix')
 parser.add_argument('-c', '--cols', type=int, default=100, help='The columns of the random game matrix')
 parser.add_argument('-g', '--gambit', type=bool, action=argparse.BooleanOptionalAction, help='If gambit=True, Gambit will be used to solve the LP. Otherwis, Nashpy will be used.')
+parser.add_argument('-m', '--games', type=str, default="random", help="Select from 10,3-Blotto, 10,4-Blotto, 10,5-Blotto, 3-move parity game 2, tic_tac_toe...")
 
 args = parser.parse_args()
 
@@ -13,6 +15,14 @@ np.set_printoptions(precision=5)
 
 rng = np.random.default_rng(1)
 row_payoff = rng.uniform(-10, 10, (args.rows, args.cols))
+
+if args.games=="random":
+    row_payoff = rng.uniform(-10, 10, (args.rows, args.cols))
+else:
+    with open("games/spinning_top_payoffs.pkl", "rb") as fh:
+        payoffs = pickle.load(fh)
+    row_payoff = payoffs[args.games]
+    
 # row_payoff = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])
 if args.gambit:
     double_oracle_with_gambit(row_payoff)
