b392c00311a6ed7d15a4039bc9460ee70594b024
Samuel Deng <zczlsde@ucl.ac.uk>
2024-01-28 00:06:37 +0000

    add_regrete_plot

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,8 @@
 import numpy as np
 import nashpy as nash
 import pygambit
+import matplotlib.pyplot as plt
+
 
 def randomize_game(payoff):
     # m = payoff.shape[0]
@@ -34,6 +36,9 @@ def double_oracle(A: np.ndarray):
     col_strategies = [i for i in range(cols) if col_flags[i]]
     # print(col_strategies)
     n = 0
+    
+    regret_row = []
+    regret_col = []
 
     while True:
         n = n + 1
@@ -48,9 +53,7 @@ def double_oracle(A: np.ndarray):
         # print(xr_row)
         xr_col = result[1]
         # print(xr_col)
-        print("Current iteration is: ", n)
-        print("Row player strategy is: ", xr_row)
-        print("Column player strategy is: ", xr_col)
+        
         # x_row is the final row player strategy 
         # assert len(xr_row) == len(row_strategies)
         x_row = np.zeros(A.shape[0])
@@ -63,9 +66,16 @@ def double_oracle(A: np.ndarray):
         x_col = np.zeros(A.shape[1])
         for i in range(len(xr_col)):
             x_col[col_strategies[i]] = xr_col[i]
+        
+        print("Current iteration is: ", n)
+        print("Row player strategy is: ", x_row)
+        print("Column player strategy is: ", x_col)
 
         # compute response values for the restricted strategies
         # TODO： Check this condition and probably reshape the x_row and x_col
+        current_value_row = x_row.T @ (A @ x_col)
+        current_value_col = x_col.T @ (-A.T @ x_row)
+        
         row_values = A @ x_col
         col_values = -A.T @ x_row
 
@@ -76,6 +86,8 @@ def double_oracle(A: np.ndarray):
         # TODO: Check the update condition again
         # max val for the row player
         value_row = row_values.max()
+        current_regret_row = value_row - current_value_row
+        regret_row.append(current_regret_row)
         for i in range(len(row_values)):
             if np.isclose(row_values[i], value_row) and row_flags[i] is False:
                 row_strategies.append(i)
@@ -85,6 +97,8 @@ def double_oracle(A: np.ndarray):
 
         # min val for the column player
         value_col = col_values.max()
+        current_regret_col = value_col - current_value_col
+        regret_col.append(current_regret_col)
         for i in range(len(col_values)):
             if np.isclose(col_values[i], value_col) and col_flags[i] is False:
                 col_strategies.append(i)
@@ -93,6 +107,19 @@ def double_oracle(A: np.ndarray):
                 break
         
         if not updated:
+            # print(regret_col)
+            # print(regret_row)
+            fig, ax = plt.subplots()
+         
+            x = [x for x in range(len(regret_row))]
+            ax.plot(x, regret_row, label='regret_row')
+            ax.plot(x, regret_col, label='regret_col')
+            ax.set_xlabel('Iteration')
+            ax.set_ylabel('Regret') 
+            ax.set_title('Regret Plot')
+            ax.legend() 
+
+            plt.show() 
             return x_row, x_col
         
 def double_oracle_with_gambit(A: np.ndarray):
@@ -110,14 +137,17 @@ def double_oracle_with_gambit(A: np.ndarray):
     col_strategies = [i for i in range(cols) if col_flags[i]]
     # print(col_strategies)
     n = 0
-
+    
+    regret_row = []
+    regret_col = []
+    
     while True:
         n = n + 1
 
         # solve the game by nashpy
         # Ar here is the matrix under the current strategies
         Ar = A[np.ix_(row_strategies, col_strategies)]
-        print(Ar)
+        # print(Ar)
         
         current_game = randomize_game(Ar)
         # strategy = pygambit.gambit.Strategy(game = current_game, player = current_game.players[0])
@@ -134,6 +164,9 @@ def double_oracle_with_gambit(A: np.ndarray):
             # print(strategy)
             prob = result.equilibria[0]._getprob_strategy(strategy)
             nash_row_prob.append(prob)
+            # print(prob)
+            # print(result.equilibria[0]._regret(strategy))
+    
             
         for strategy in current_game.players[1].strategies:
             # print(strategy)
@@ -143,13 +176,11 @@ def double_oracle_with_gambit(A: np.ndarray):
         # print(result.equilibria[0]["Row"])
         # matching_pennies = nash.Game(Ar)
         # result = matching_pennies.linear_program()
+        
         xr_row = np.array(nash_row_prob)
         # # print(xr_row)
         xr_col = np.array(nash_col_prob)
         # # print(xr_col)
-        print("Current iteration is: ", n)
-        print("Row player strategy is: ", xr_row)
-        print("Column player strategy is: ", xr_col)
         # x_row is the final row player strategy 
         # assert len(xr_row) == len(row_strategies)
         x_row = np.zeros(A.shape[0])
@@ -162,9 +193,17 @@ def double_oracle_with_gambit(A: np.ndarray):
         x_col = np.zeros(A.shape[1])
         for i in range(len(xr_col)):
             x_col[col_strategies[i]] = xr_col[i]
+        
+        
+        print("Current iteration is: ", n)
+        print("Row player strategy is: ", x_row)
+        print("Column player strategy is: ", x_col)
 
         # compute response values for the restricted strategies
         # TODO： Check this condition and probably reshape the x_row and x_col
+        current_value_row = x_row.T @ (A @ x_col)
+        current_value_col = x_col.T @ (-A.T @ x_row)
+        
         row_values = A @ x_col
         col_values = -A.T @ x_row
 
@@ -175,6 +214,9 @@ def double_oracle_with_gambit(A: np.ndarray):
         # TODO: Check the update condition again
         # max val for the row player
         value_row = row_values.max()
+        current_regret_row = value_row - current_value_row
+        regret_row.append(current_regret_row)
+        # print(current_regret_row)
         for i in range(len(row_values)):
             if np.isclose(row_values[i], value_row) and row_flags[i] is False:
                 row_strategies.append(i)
@@ -184,6 +226,9 @@ def double_oracle_with_gambit(A: np.ndarray):
 
         # min val for the column player
         value_col = col_values.max()
+        current_regret_col = value_col - current_value_col
+        regret_col.append(current_regret_col)
+        # print(current_regret_col)
         for i in range(len(col_values)):
             if np.isclose(col_values[i], value_col) and col_flags[i] is False:
                 col_strategies.append(i)
@@ -192,5 +237,20 @@ def double_oracle_with_gambit(A: np.ndarray):
                 break
         
         if not updated:
+            # print(regret_col)
+            # print(regret_row)
+            fig, ax = plt.subplots()
+         
+            x = [x for x in range(len(regret_row))]
+            ax.plot(x, regret_row, label='regret_row')
+            ax.plot(x, regret_col, label='regret_col')
+            ax.set_xlabel('Iteration')
+            ax.set_ylabel('Regret') 
+            ax.set_title('Regret Plot')
+            ax.legend() 
+
+            plt.show() 
+
             return x_row, x_col
+    
     
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import argparse
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--rows', type=int, default=10, help='The rows of the random game matrix')
-parser.add_argument('-c', '--cols', type=int, default=10, help='The columns of the random game matrix')
+parser.add_argument('-r', '--rows', type=int, default=100, help='The rows of the random game matrix')
+parser.add_argument('-c', '--cols', type=int, default=100, help='The columns of the random game matrix')
 
 args = parser.parse_args()
 
@@ -12,6 +12,6 @@ np.set_printoptions(precision=5)
 
 rng = np.random.default_rng(1)
 row_payoff = rng.uniform(-10, 10, (args.rows, args.cols))
-row_payoff = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])
+# row_payoff = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])
 # double_oracle(row_payoff)
-double_oracle_with_gambit(row_payoff)
+# double_oracle_with_gambit(row_payoff)
